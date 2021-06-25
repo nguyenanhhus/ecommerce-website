905c6d2725389a63fea8553f3224005d5f908ee1
nguyenanhhus <nguyenanh.binoo@gmail.com>
2021-06-25 22:55:05 +0700

    add: create repo

---
+++ js/index.js
@@ -0,0 +1,38 @@
+const menu = document.querySelector('.menu');
+const navOpen = document.querySelector('.hamburger');
+const navClose = document.querySelector('.close');
+const navBar = document.querySelector('.nav');
+
+const navLeft = menu.getBoundingClientRect().left;
+
+navOpen.addEventListener('click', function() {
+    if (navLeft < 0) {
+        menu.classList.add('show');
+        document.body.classList.add('show'); 
+    }
+});
+
+navClose.addEventListener('click', function() {
+    menu.classList.remove('show');
+    document.body.classList.remove('show');
+        
+});
+
+// Fix nav
+const navHeight = navBar.getBoundingClientRect().height;
+
+window.addEventListener('scroll', () => {
+    const scrollHeight = window.pageYOffset;
+    if (scrollHeight > navHeight){
+        navBar.classList.add('fix-nav');
+    } else {
+        navBar.classList.remove('fix-nav');
+    }
+});
+
+gsap.from(".logo", { opacity: 0, duration: 1, delay: 0.5, y: -10 });
+gsap.from(".hamburger", { opacity: 0, duration: 1, delay: 1, x: 20 });
+gsap.from(".hero-img", { opacity: 0, duration: 1, delay: 1.5, x: -200 });
+gsap.from(".hero-content h2", { opacity: 0, duration: 1, delay: 2, y: -50 });
+gsap.from(".hero-content h1", { opacity: 0, duration: 1, delay: 2.5, y: -45 });
+gsap.from(".hero-content a", { opacity: 0, duration: 1, delay: 3.5, y: 50 });
